ff20b5f37f1a395003370238c192bc7021ff21de
Bahar Pala <109612572+mcd2023@users.noreply.github.com>
2026-05-25 22:07:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const slides = document.querySelectorAll('.slide');
+
+const next = document.querySelector('.next');
+
+const prev = document.querySelector('.prev');
+
+let index = 0;
+
+function showSlide(i){
+
+  slides.forEach(slide => {
+    slide.classList.remove('active');
+  });
+
+  slides[i].classList.add('active');
+}
+
+next.addEventListener('click', () => {
+
+  index++;
+
+  if(index >= slides.length){
+    index = 0;
+  }
+
+  showSlide(index);
+
+});
+
+prev.addEventListener('click', () => {
+
+  index--;
+
+  if(index < 0){
+    index = slides.length - 1;
+  }
+
+  showSlide(index);
+
+});
+
+setInterval(() => {
+
+  index++;
+
+  if(index >= slides.length){
+    index = 0;
+  }
+
+  showSlide(index);
+
+}, 5000);
